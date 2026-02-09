8d4560140957bcbdf8f6f9e9525f03aeda2b3b2c
Filip Weiss <me@fiws.net>
2026-02-09 12:16:52 +0100

    pnpm update

--- src/scratchpad/setup.ts
+++ src/scratchpad/setup.ts
@@ -1,4 +1,6 @@
 import { PlaywrightEnvironment } from "effect-playwright/experimental";
 import { chromium } from "playwright-core";
 
-export const liveLayer = PlaywrightEnvironment.layer(chromium);
+export const liveLayer = PlaywrightEnvironment.layer(chromium, {
+  headless: false,
+});
